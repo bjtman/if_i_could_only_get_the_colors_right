8878b303484ef0ee153f20de26851a5334338d75
Brian Tice <briantice@Brians-MacBook-Pro.local>
2020-01-05 13:07:19 -0500

    Added a button, and added another color scheme. The button toggles between the color palletes.

--- sketch.js
+++ sketch.js
@@ -3,22 +3,38 @@
  * @name If I Could Just Get The Colors Right
  * @description (Expansion of the work of Sanzo Wada. .
  */
+
+let STATE = 0;
+
+
+
+
 function setup() {
   createCanvas(710, 400);
   noStroke();
-  background(color('#a4986b'));
+  
+  
+  button = createButton('Change');
+  button.position(width - 80, height - 30);
+  button.mousePressed(change_state);
 
   //let inside = color(204, 102, 0);
   //let middle = color(204, 153, 0);
   //let outside = color(153, 51, 0);
-
-  // These statements are equivalent to the statements above.
-  // Programmers may use the format they prefer.
   let raw_sienna = color('#b85e00');
   let ivory_buff = color('#a4986b');
   let olive_green = color('#556934');
   let slate_color = color('#1b3644');
-
+  
+  let apricot_yellow = color('ffe600');
+  let yellow_orange = color('ff8c00');
+  let cossack_green = color('328e13');
+  // These statements are equivalent to the statements above.
+  // Programmers may use the format they prefer.
+  
+  
+  background(color('#a4986b'));
+  
   push();
   translate(60, 80);
   fill(raw_sienna);
@@ -57,3 +73,110 @@ function setup() {
   
   
 }
+
+
+function change_state() {
+  
+  let raw_sienna = color('#b85e00');
+  let ivory_buff = color('#a4986b');
+  let olive_green = color('#556934');
+  let slate_color = color('#1b3644');
+  
+  let apricot_yellow = color('#ffe600');
+  let yellow_orange = color('#ff8c00');
+  let cossack_green = color('#328e13');
+  
+  
+  if(STATE == 0) {
+    
+    
+    
+    background(color('#328e13'));
+  
+    push();
+    translate(60, 80);
+    fill(apricot_yellow);
+    rect(0, 0, 100, 400);
+    fill(cossack_green);
+    rect(40, 60, 120, 120);
+    fill(raw_sienna);
+    rect(60, 90, 80, 80);
+    fill(yellow_orange);
+    rect(0,-7,100,-10);
+    pop();
+
+    push();
+    translate(250, 80);
+    fill(yellow_orange);
+    rect(0, 0, 100, 400);
+    fill(cossack_green);
+    rect(40, 60, 120, 120);
+    fill(cossack_green);
+    rect(60, 90, 80, 80);
+    fill(raw_sienna);
+    rect(0,-7,100,-10);
+    pop();
+  
+    push();
+    translate(440, 80);
+    fill(raw_sienna);
+    rect(0, 0, 100, 400);
+    fill(cossack_green);
+    rect(40, 60, 120, 120);
+    fill(yellow_orange);
+    rect(60, 90, 80, 80);
+    fill(apricot_yellow);
+    rect(0,-7,100,-10);
+    pop();
+      
+    
+    text('state change to 1, translation from dblodorn',10,10);
+    STATE = 1;
+  }
+  else if(STATE == 1) {
+    
+    
+    background(color('#a4986b'));
+  
+    push();
+    translate(60, 80);
+    fill(raw_sienna);
+    rect(0, 0, 100, 400);
+    fill(ivory_buff);
+    rect(40, 60, 120, 120);
+    fill(olive_green);
+    rect(60, 90, 80, 80);
+    fill(slate_color);
+    rect(0,-7,100,-10);
+    pop();
+
+    push();
+    translate(250, 80);
+    fill(slate_color);
+    rect(0, 0, 100, 400);
+    fill(ivory_buff);
+    rect(40, 60, 120, 120);
+    fill(raw_sienna);
+    rect(60, 90, 80, 80);
+    fill(olive_green);
+    rect(0,-7,100,-10);
+    pop();
+  
+    push();
+    translate(440, 80);
+    fill(olive_green);
+    rect(0, 0, 100, 400);
+    fill(ivory_buff);
+    rect(40, 60, 120, 120);
+    fill(ivory_buff);
+    rect(60, 90, 80, 80);
+    fill(raw_sienna);
+    rect(0,-7,100,-10);
+    pop();
+  
+    text('state change to 0, hand translation',10,10);
+    STATE = 0;
+  }
+  
+}  
+  
